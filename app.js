f9e2ecd1621dba46c7f56ba6e54ed32221fa06b8
Abid Ali <arcotabidali99@gmail.com>
2020-07-08 23:54:27 +0530

    very first time deploying fitfury to heroku

---
+++ app.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require('lodash');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin-abid:Abidpass@cluster0.vlyam.mongodb.net/FitfuryDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// const contentSchema = new Schema({}, { strict: false });
+//    const Content = mongoose.model('Content', contentSchema);
+  // const content  = new Content({ iAmNotInTheSchema: true });
+  //  thing.save() // iAmNotInTheSchema is now saved to the db!!
+
+  var Post = mongoose.model('Post', {
+    title : String,
+    intro : String,
+    content:String
+  });
+
+
+
+
+
+
+app.get("/",function(req,res){
+
+Post.find(function(err,postsArr){
+  if(err){console.log("no error encountered");}
+  else{
+    postsArr.forEach(function(post){
+      console.log(post.content);
+    })
+   res.render("home",{startingContent : homeStartingContent, posts:postsArr});
+    }
+    })
+
+
+});
+
+
+
+
+app.get("/about",function(req,res){
+
+res.render("about",{aboutContent :aboutContent});
+
+})
+
+
+
+app.get("/contact",function(req,res){
+
+res.render("contact",{contactContent : contactContent});
+
+})
+
+
+
+app.get("/compose",function(req,res){
+
+res.render("compose");
+
+})
+
+app.post("/compose",function(req,res){
+
+
+  post1 = {
+    title : req.body.postTitle,
+    intro : req.body.postIntro,
+    content : req.body.content
+  }
+
+const post = new Post(post1);
+post.save()
+
+
+
+
+
+res.redirect("/");
+})
+
+
+
+app.get("/posts/:post", function(req,res) {
+
+const postReq =_.lowerCase(req.params.post);
+
+
+var sendPost;
+var postSent="false";
+Post.find(function(err,postsArr){
+  if(err){console.log("no error encountered");}
+  else{
+    postsArr.forEach(function(post){
+    var postTitle = _.lowerCase(post.title);
+    if(postTitle==postReq){
+       sendPost = post;
+       postSent="true";
+        res.render("post",{displayPost:sendPost});
+    }
+
+  });
+}});
+
+});
+
+
+app.route("/compose/:articleTitle")
+.delete(function(req,res){
+  Post.deleteOne({title :req.params.articleTitle},function(err){
+    if(!err){
+      res.send("succesfully delete the article " + req.params.articleTitle);
+    }
+    else{
+      res.send(err);
+    }
+  })
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
